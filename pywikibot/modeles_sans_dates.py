116f1aa0a40b676306db855ae0262cbece02624a
NaggoBot <naggobot@vps-d0105536.vps.ovh.net>
2021-11-19 21:57:09 +0000

    update to python3

--- pywikibot/modeles_sans_dates.py
+++ pywikibot/modeles_sans_dates.py
@@ -37,43 +37,43 @@ import locale
 
 class BotPrecisionModele:
 	def __init__(self):
-		self.titres_modeles_initiaux = [u"À sourcer", u"Admissibilité à vérifier", u"À wikifier", \
-			u"Article sans source", u"Vérifiabilité", u"Orphelin", u"Guide pratique", u"Section guide pratique", \
-			u"Section guide pratique", u"Section à wikifier", u"Section à sourcer", u"Section à délister", \
-			u"BPV à sourcer", u"À illustrer", u"Section à sourcer", u"Sources secondaires", u"Sources à lier", \
-			u"Internationaliser", u"À mettre à jour", u"À recycler", u"Source unique", \
-			u"Traduction à revoir", u"Travail inédit", u"Style non encyclopédique", u"Style non encyclopédique section", \
-			u"Trop d'ouvrages", u"Résumé introductif", u"Désaccord de neutralité", u"Traduction incomplète", \
-			u"Rédaction", u"Anecdotes", u"Trop de liens", u"À dater", u"À délister", u"Désaccord de neutralité", \
-			u"Pertinence section", u"À vérifier", u"Section non neutre", u"Rédaction", u"Trop d'ouvrages", \
-			u"À déjargoniser", u"Promotionnel", u"Article incomplet", u"À vérifier/architecture", u"Trop long", \
-			u"Article non neutre", u"Conventions bibliographiques", u"Section trop longue", u"À recycler/droit", \
-			u"Section à recycler", u"À recycler/biographie", u"À recycler/catch", u"À recycler/géographie", \
-			u"À recycler/histoire militaire", u"À recycler/judaïsme", u"À recycler/mathématiques", \
-			u"À recycler/récompenses", u"À recycler/zoologie", \
-			u"À vérifier/alimentation", u"À vérifier/anthropologie", u"À vérifier/architecture", \
-			u"À vérifier/association", u"À vérifier/astronomie", u"À vérifier/biographie", u"À vérifier/biologie", \
-			u"À vérifier/botanique", u"À vérifier/chimie", u"À vérifier/cinéma", u"À vérifier/criminologie", \
-			u"À vérifier/culture", u"À vérifier/danse", u"À vérifier/droit", u"À vérifier/entreprise", \
-			u"À vérifier/environnement", u"À vérifier/généalogie", u"À vérifier/géographie", u"À vérifier/géologie", \
-			u"À vérifier/hip-hop", u"À vérifier/histoire", u"À vérifier/histoire militaire", u"À vérifier/informatique", \
-			u"À vérifier/jeu vidéo", u"À vérifier/linguistique", u"À vérifier/littérature", u"À vérifier/mathématiques", \
-			u"À vérifier/mode", u"À vérifier/musique", u"À vérifier/mythologie", u"À vérifier/médecine", \
-			u"À vérifier/média", u"À vérifier/paléontologie", u"À vérifier/peinture", u"À vérifier/philosophie", \
-			u"À vérifier/physique", u"À vérifier/politique", u"À vérifier/psychologie", u"À vérifier/religion", \
-			u"À vérifier/sciences", u"À vérifier/sociologie", u"À vérifier/sport", u"À vérifier/transports", \
-			u"À vérifier/télévision", u"À vérifier/zoologie", u"À vérifier/économie", u"À vérifier/éducation", \
-			u"Plan", u"Sources obsolètes", u"Typographie", u"Catalogue de vente", u"Hagiographique", u"CV", \
-			u"À prouver", u"À désacadémiser", u"Pertinence", u"Copyvio", u"À délister", u"Section à recycler", \
-			u"Résumé introductif trop long", u"Vie privée", u"à recycler/histoire militaire", u"pour wikiquote", \
-			u"trop de citations", u"Anthropocentrisme", u"Résumé introductif trop court",
-			u"Section trop compacte", u"Orthographe", u"Délister", u"Article mal proportionné"]
+		self.titres_modeles_initiaux = ["À sourcer", "Admissibilité à vérifier", "À wikifier", \
+			"Article sans source", "Vérifiabilité", "Orphelin", "Guide pratique", "Section guide pratique", \
+			"Section guide pratique", "Section à wikifier", "Section à sourcer", "Section à délister", \
+			"BPV à sourcer", "À illustrer", "Section à sourcer", "Sources secondaires", "Sources à lier", \
+			"Internationaliser", "À mettre à jour", "À recycler", "Source unique", \
+			"Traduction à revoir", "Travail inédit", "Style non encyclopédique", "Style non encyclopédique section", \
+			"Trop d'ouvrages", "Résumé introductif", "Désaccord de neutralité", "Traduction incomplète", \
+			"Rédaction", "Anecdotes", "Trop de liens", "À dater", "À délister", "Désaccord de neutralité", \
+			"Pertinence section", "À vérifier", "Section non neutre", "Rédaction", "Trop d'ouvrages", \
+			"À déjargoniser", "Promotionnel", "Article incomplet", "À vérifier/architecture", "Trop long", \
+			"Article non neutre", "Conventions bibliographiques", "Section trop longue", "À recycler/droit", \
+			"Section à recycler", "À recycler/biographie", "À recycler/catch", "À recycler/géographie", \
+			"À recycler/histoire militaire", "À recycler/judaïsme", "À recycler/mathématiques", \
+			"À recycler/récompenses", "À recycler/zoologie", \
+			"À vérifier/alimentation", "À vérifier/anthropologie", "À vérifier/architecture", \
+			"À vérifier/association", "À vérifier/astronomie", "À vérifier/biographie", "À vérifier/biologie", \
+			"À vérifier/botanique", "À vérifier/chimie", "À vérifier/cinéma", "À vérifier/criminologie", \
+			"À vérifier/culture", "À vérifier/danse", "À vérifier/droit", "À vérifier/entreprise", \
+			"À vérifier/environnement", "À vérifier/généalogie", "À vérifier/géographie", "À vérifier/géologie", \
+			"À vérifier/hip-hop", "À vérifier/histoire", "À vérifier/histoire militaire", "À vérifier/informatique", \
+			"À vérifier/jeu vidéo", "À vérifier/linguistique", "À vérifier/littérature", "À vérifier/mathématiques", \
+			"À vérifier/mode", "À vérifier/musique", "À vérifier/mythologie", "À vérifier/médecine", \
+			"À vérifier/média", "À vérifier/paléontologie", "À vérifier/peinture", "À vérifier/philosophie", \
+			"À vérifier/physique", "À vérifier/politique", "À vérifier/psychologie", "À vérifier/religion", \
+			"À vérifier/sciences", "À vérifier/sociologie", "À vérifier/sport", "À vérifier/transports", \
+			"À vérifier/télévision", "À vérifier/zoologie", "À vérifier/économie", "À vérifier/éducation", \
+			"Plan", "Sources obsolètes", "Typographie", "Catalogue de vente", "Hagiographique", "CV", \
+			"À prouver", "À désacadémiser", "Pertinence", "Copyvio", "À délister", "Section à recycler", \
+			"Résumé introductif trop long", "Vie privée", "à recycler/histoire militaire", "pour wikiquote", \
+			"trop de citations", "Anthropocentrisme", "Résumé introductif trop court",
+			"Section trop compacte", "Orthographe", "Délister", "Article mal proportionné"]
 		self.site = pywikibot.Site()
-		self.liste_titres_cats = [u"Admissibilité à vérifier, date manquante", u"Article à wikifier, date manquante", \
-			u"Article sans source, date manquante", u"Article non vérifiable, date manquante", \
-			u"Article orphelin, date manquante", u"Guide pratique, date manquante", \
-			u"Article manquant de références depuis date inconnue", u"Article avec section à délister, date manquante", \
-			u"Bandeau de maintenance sans paramètre date", u"Bandeau de maintenance sans paramètre date/Liste complète"]
+		self.liste_titres_cats = ["Admissibilité à vérifier, date manquante", "Article à wikifier, date manquante", \
+			"Article sans source, date manquante", "Article non vérifiable, date manquante", \
+			"Article orphelin, date manquante", "Guide pratique, date manquante", \
+			"Article manquant de références depuis date inconnue", "Article avec section à délister, date manquante", \
+			"Bandeau de maintenance sans paramètre date", "Bandeau de maintenance sans paramètre date/Liste complète"]
 		#u"Article avec section à wikifier, date manquante"
 
 	def generate_liste_pages_modele(self, modeles):
@@ -86,7 +86,7 @@ class BotPrecisionModele:
 			while page.isRedirectPage():
 				page = page.getRedirectTarget()
 			liste_titres.append(page)
-			liste_titres_redirect = [p for p in page.backlinks(filterRedirects=True)]
+			liste_titres_redirect = [p for p in page.backlinks(filter_redirects=True)]
 			# ex : liste_titres_redirect =
 			#			[u'Sources', u'A sourcer', u'Source ?']
 			liste_titres.extend(liste_titres_redirect)
@@ -100,11 +100,16 @@ class BotPrecisionModele:
 		site = pywikibot.Site()
 
 		unblock_found = True
-		history = page.getVersionHistory()
+		history = page.revisions()
 
-		pywikibot.output(u"Analysing page %s" % page.title())
-		if len(history) == 1:
-			[(id, timestamp, user, comment)] = history
+		pywikibot.output("Analysing page %s" % page.title())
+#		if len(history) == 1:
+#			[(id, timestamp, user, comment)] = history
+#			return (timestamp, id)
+		if page.revision_count() == 1:
+			rev=next(history)
+			timestamp = rev['timestamp']
+			id = rev['revid']
 			return (timestamp, id)
 
 		oldid = None
@@ -113,7 +118,12 @@ class BotPrecisionModele:
 		look_back_count = 2
 		look_back = look_back_count
 
-		for (id, timestamp, user, comment) in history:
+		for rev in history:
+			user = rev['user']
+			comment = rev['comment']
+			timestamp = rev['timestamp']
+			id = rev['revid']
+
 			pywikibot.output("Analyzing id %i: timestamp is %s and user is %s" % (id, timestamp, user+(" (lookback)" if look_back < look_back_count else "")))
 			if not user:
 				pywikibot.output("User's missing (hidden?), skipping this version...")
@@ -121,7 +131,7 @@ class BotPrecisionModele:
 
 			text = page.getOldVersion(id)
 			if not text:
-				pywikibot.output(u"Can't get rev text (hidden?), skipping this version...")
+				pywikibot.output("Can't get rev text (hidden?), skipping this version...")
 				continue
 			# text might be too long, if so textlib.extract_templates_and_params won't
 			# proceed and will skip some templates
@@ -139,9 +149,9 @@ class BotPrecisionModele:
 						unblock_found = True
 						look_back = look_back_count
 						break
-				except Exception, myexception:
-					pywikibot.output(u'An error occurred while analyzing template %s' % template_name)
-					pywikibot.output(u'%s %s'% (type(myexception), myexception.args))
+				except Exception as myexception:
+					pywikibot.output('An error occurred while analyzing template %s' % template_name)
+					pywikibot.output('%s %s'% (type(myexception), myexception.args))
 
 			#if oldid:
 			#	print("oldid is %i" % oldid)
@@ -178,7 +188,7 @@ class BotPrecisionModele:
 
 	def operate(self, page, liste_modeles):
 		dict = {}
-		modeles_modifies_str = u""
+		modeles_modifies_str = ""
 		count_modeles_modifies = 0
 		add_date = False
 
@@ -197,21 +207,21 @@ class BotPrecisionModele:
 
 		for modele in dict:
 		# NB : modele est de type pywikibot.Page
-			titre_modele = modele.title(withNamespace=False)
+			titre_modele = modele.title(with_ns=False)
 
 			add_date = True
 			change_date = False
-			re_params = u""
+			re_params = ""
 			#str_params = u""
 			#pywikibot.output(u"modele : %s" % modele.title(withNamespace=False))
 			if dict[modele]:
 				for param in dict[modele]:
 					param = param.replace('(', '\(').replace(')', '\)').replace('[', '\[').replace(']', '\]').replace('{', '\{').replace('{', '\}').replace('?', '\?').replace('|', '\|').replace('*', '\*').replace('+', '\+')
 					#param = re.escape(param)
-					re_params += (u" *\| *([0-9]+ *= *)?%s" % param) # Paramètres allant avec le modèle
+					re_params += (" *\| *([0-9]+ *= *)?%s" % param) # Paramètres allant avec le modèle
 					#str_params += u"|%s" % param
-					if re.search(u"^date *=.+", param):
-						param_date = re.search(u"^date *= *([0-9]{0,2}) *(janvier|février|mars|avril|mai|juin|juillet|août|septembre|octobre|novembre|décembre) *(20[0-9]{2})", param)
+					if re.search("^date *=.+", param):
+						param_date = re.search("^date *= *([0-9]{0,2}) *(janvier|février|mars|avril|mai|juin|juillet|août|septembre|octobre|novembre|décembre) *(20[0-9]{2})", param)
 						add_date = False
 						if param_date and param_date.group(1):
 							text = text.replace(param_date.group(0), "date=%s %s" % (param_date.group(2), param_date.group(3)))
@@ -231,10 +241,10 @@ class BotPrecisionModele:
 			if add_date:
 				# Permet de rechercher le modèle (avec ses paramètres) dans le texte
 				#pywikibot.output(u"{ *{ *([%s%s]%s%s) *} *}" % (titre_modele[0].lower(), titre_modele[0].upper(), titre_modele[1:].replace(' ', '[ _]'), unicode(re_params)))
-				str_re_titre_modele = u"{ *{ *([%s%s]%s%s) *} *}" % (titre_modele[0].lower(), titre_modele[0].upper(), titre_modele[1:].replace(' ', '[ _]+'), unicode(re_params))
+				str_re_titre_modele = "{ *{ *([%s%s]%s%s) *} *}" % (titre_modele[0].lower(), titre_modele[0].upper(), titre_modele[1:].replace(' ', '[ _]+'), str(re_params))
 
 				(timestamp, id) = self.find_add(page, modele)
-				date = timestamp.strftime(u"%B %Y")
+				date = timestamp.strftime("%B %Y")
 				#print "found the add : %s (%i) -> %s" % (timestamp, id, date)
 				if date is None:
 					pywikibot.output("Error, date is None!")
@@ -247,19 +257,19 @@ class BotPrecisionModele:
 					pywikibot.output("An error occurred: no match for %s" % str_re_titre_modele)
 					return
 				#pywikibot.output(match_modele.group(1))
-				new = u"{{%s|date=%s}}" % (match_modele.group(1), date.decode('utf-8'))
+				new = "{{%s|date=%s}}" % (match_modele.group(1), date)
 				#pywikibot.output(new)
 
 				text = re.sub(str_re_titre_modele, new, text)
 				#text = text.replace(match_modele.group(0), new)
-				modeles_modifies_str += u"[[Modèle:%(modele)s|{{%(modele)s}}]], " % {'modele':titre_modele}
+				modeles_modifies_str += "[[Modèle:%(modele)s|{{%(modele)s}}]], " % {'modele':titre_modele}
 				count_modeles_modifies += 1
 
 		modeles_modifies_str = modeles_modifies_str[0:-2] # Enlever le ', ' en trop
 		if count_modeles_modifies == 1:
-			comment = u"Bot: Ajout du paramètre 'date' dans le modèle %s" % modeles_modifies_str
+			comment = "Bot: Ajout du paramètre 'date' dans le modèle %s" % modeles_modifies_str
 		else:
-			comment = u"Bot: Ajout du paramètre 'date' dans les modèles %s" % modeles_modifies_str
+			comment = "Bot: Ajout du paramètre 'date' dans les modèles %s" % modeles_modifies_str
 
 		pywikibot.showDiff(page.get(), text)
 		if old_text != page.get():
@@ -269,7 +279,7 @@ class BotPrecisionModele:
 			page.put(text, comment = comment)
 
 	def run(self):
-		pywikibot.output(u"Initialisation en cours… date : %s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+		pywikibot.output("Initialisation en cours… date : %s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 		t1 = time.time()
 
 		# Génère la liste des pages à traiter
@@ -279,7 +289,7 @@ class BotPrecisionModele:
 			liste_pages_a_traiter.extend([page for page in cat.articles(content=True, namespaces=[0])])
 
 		if not liste_pages_a_traiter:
-			pywikibot.output(u"Aucune page à traiter")
+			pywikibot.output("Aucune page à traiter")
 			return
 
 		# Génère la liste complète des modèles utilisables (redirections comprises)
@@ -288,10 +298,10 @@ class BotPrecisionModele:
 		for modele in self.liste_pages_modeles: pywikibot.output(modele)
 
 		t2 = time.time()
-		pywikibot.output(u"Initialisation terminée. Temps mis : %i secondes" % int(t2-t1))
+		pywikibot.output("Initialisation terminée. Temps mis : %i secondes" % int(t2-t1))
 
 		if debug:
-			liste_pages_a_traiter = [pywikibot.Page(self.site, u'Utilisateur:Toto Azéro/Bac à sable 5')]
+			liste_pages_a_traiter = [pywikibot.Page(self.site, 'Utilisateur:Toto Azéro/Bac à sable 5')]
 
 		# Analyse page par page
 		for page in liste_pages_a_traiter:
@@ -316,7 +326,7 @@ if __name__ == "__main__":
 
 	try:
 		main()
-	except Exception, myexception:
+	except Exception as myexception:
 		#print u'%s %s'% (type(myexception), myexception.args)
 		if not debug:
 			_errorhandler.handle(myexception)
